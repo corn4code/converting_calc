535a853c271b504675ba535e4776c6850004e05d
corn4code <cornelius_weigert@outlook.de>
2021-04-10 16:15:24 +0200

    added Fahrenheit

--- main.py
+++ main.py
@@ -10,14 +10,9 @@ print(" help: shows commands")
 print(" overview: shows conversion modules")
 print(" option + module abbreviation: shows details to conversions")
 print(" quit(): end program")
-# print("Overview: ")
-# print("  t: Temperature ")
-# print("    t1 Celsius to Kelvin")
-# print("    t2 Celsius to Fahrenheit")
-# print("    t3 Kelvin to Celsius")
-# print("    t4 Kelvin to Fahrenheit")
+
 while True:
-    input = str(input("enter command: "))
+    input = input(">> ")
     # get_input()
     if input == "help":
         print("Commands:")
@@ -25,37 +20,45 @@ while True:
         print(" overview: shows conversion modules")
         print(" option + module abbreviation")
 
-    if input == "overview":
+    elif input == "overview":
         print("Modules:")
         print("  t: Temperature")
 
-    if input == "option t":
+    elif input == "option t":
         print("Temperature ")
         print("  t1 Celsius to Kelvin")
         print("  t2 Celsius to Fahrenheit")
         print("  t3 Kelvin to Celsius")
         print("  t4 Kelvin to Fahrenheit")
+        print("  t5 Fahrenheit to Celsius")
+        print("  t6 Fahrenheit to Kelvin")
 
-
-
-    # choice = input
-
-    if input == "t1":
+    elif input == "t1":
         teperatur = float(input("Enter temperature in Celsius: "))
         print(temperatur_set_up.Celsius_to_Kelvin(teperatur))
 
-    if input == "t2":
+    elif input == "t2":
         teperatur = float(input("Enter temperature in Celsius"))
         print(temperatur_set_up.Celsius_to_Fahrenheit(teperatur))
 
-    if input == "t3":
+    elif input == "t3":
         temperatur = float(input("Enter temperature in Kelvin: "))
         print(temperatur_set_up.Kelvin_to_Celsius(temperatur))
 
-    if input == "t4":
+    elif input == "t4":
         temperatur = float(input("Enter temperature in Kelvin: "))
         print(temperatur_set_up.Kelvin_to_Fahrenheit(temperatur))
 
-    if input == "quit()":
+    elif input == "t5":
+        temperatur = float(input("Enter temperature in Fahrenheit: "))
+        print(temperatur_set_up.Fahrenheit_to_Celvin(temperatur))
+
+    elif input == "t6":
+        temperatur = float(input("Enter temperature in Fahrenheit: "))
+        print(temperatur_set_up.Fahrenheit_to_Kelvin(temperatur))
+
+    elif input == "quit()":
         break
 
+    else:
+        print("invalid input")
--- temperatur_set_up.py
+++ temperatur_set_up.py
@@ -19,3 +19,15 @@ def Kelvin_to_Fahrenheit(temperatur):
     global result
     result = ((temperatur - 273) * 1.8) + 32
     return result
+
+
+def Fahrenheit_to_Celvin(temperatur):
+    global result
+    result = ((temperatur - 32) / 1.8)
+    return result
+
+
+def Fahrenheit_to_Kelvin(temperatur):
+    global result
+    result = ((temperatur +459.67) / 1.8)
+    return result
